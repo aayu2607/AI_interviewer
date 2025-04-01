c96788f89ea30c266a1bea9c5a71d88c476f7408
aayu2607 <ayushmahalle2607@gmail.com>
2025-04-01 19:24:24 +0530

    implement auth and storage

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,26 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp,getApp,getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase-admin/firestore";
-import { getAuth } from "firebase-admin/auth";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBczmE1crChFOJQfkw58bq_6kbJT9QlYhI",
-  authDomain: "ai-interviewer-e8a88.firebaseapp.com",
-  projectId: "ai-interviewer-e8a88",
-  storageBucket: "ai-interviewer-e8a88.firebasestorage.app",
-  messagingSenderId: "518924435284",
-  appId: "1:518924435284:web:290ef3ffc36a5d192a92c1",
-  measurementId: "G-9ZTCW975TJ"
+  apiKey: "AIzaSyAo8YCXhmqa9nhoPVVyLxtaBBBr9YwdzQM",
+  authDomain: "ai-interviewer-66f10.firebaseapp.com",
+  projectId: "ai-interviewer-66f10",
+  storageBucket: "ai-interviewer-66f10.firebasestorage.app",
+  messagingSenderId: "1025369584903",
+  appId: "1:1025369584903:web:bda257209f6c24209281a2",
+  measurementId: "G-B6DLNK5N3Q"
 };
 
 // Initialize Firebase
 const app =!getApps.length ? initializeApp(firebaseConfig) : getApp();
-const analytics = getAnalytics(app);
 
-export const auth=getAuth(app);
-export const db=getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
